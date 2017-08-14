58f2cb08af8a66ce715db9690b3a39bbf203cc84
george <george@macbook-2.local>
2017-08-13 19:28:16 -0600

    change to port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const _gen = require('@g3org3/generator')(__dirname)
 // My Custom Generator
 const context = {
   image: 'registry.jorgeadolfo.com/name',
-  ports: '8000'
+  port: '8000'
 }
 
 _gen.cloneTemplateStructure(context)
